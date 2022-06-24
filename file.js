56b96bd42a96a3d8d1345bca4503e1e729bc872f
Oluwafemi Makanjuola <makanjuolafemi17@gmail.com>
2022-06-24 08:04:32 +0100

    Done

---
+++ file.js
@@ -0,0 +1,5 @@
+const myName = "Makanjuola Oluwafemi";
+const myHeight = "198 cm";
+const myCountry = "Nigeria";
+
+alert(`My name is ${myName}\nMy height is ${myHeight}\nMy country is ${myCountry}`)
